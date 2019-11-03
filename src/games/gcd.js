fa1fa7789b380d31c0c523d7418b4bdc80dc7bbd
Alex <al.ryabchikov@gmail.com>
2019-11-03 20:32:14 +0400

    Step 9.1

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,15 +5,16 @@ const rules = 'Find the greatest common divisor of given numbers.';
 const getNumber1 = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getNumber2 = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+const getCorrectAnswer = (num1, num2) => {
+  if (num2 === 0) return num1;
+  return getCorrectAnswer(num2, num1 % num2);
+};
+
 const answerNumber = () => {
-  let num1 = getNumber1(1, 100);
-  let num2 = getNumber2(1, 100);
-  const getCorrectAnswer = () => {
-    while (num2 !== 0) num2 = num1 % (num1 = num2);
-    return num1;
-  };
+  const num1 = getNumber1(1, 100);
+  const num2 = getNumber2(1, 100);
   const number = (`${num1} ${num2}`);
-  const correctAnswer = String(getCorrectAnswer());
+  const correctAnswer = String(getCorrectAnswer(num1, num2));
   return data(correctAnswer, number);
 };
 
